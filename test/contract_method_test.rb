75212bed6d80b69a1964c411dd5a2022fdbb1f1a
Kyle Wiest <kylew@bluebottlecoffee.com>
2020-06-18 13:56:10 -0700

    Use meaningful parameter names

--- test/contract_method_test.rb
+++ test/contract_method_test.rb
@@ -8,35 +8,35 @@ class ContactMethodTest < Minitest::Test
     @contract_method = CBRAContracts::ContractMethod.new(
       :test_method, 'A method to test param types and schema', self.class
     )
-    @contract_method.param(:p1, :string, 'String param')
-    @contract_method.param(:p2, :integer, 'Integer param')
-    @contract_method.param(:p3, :float, 'Float param')
-    @contract_method.param(:p4, :symbol, 'Symbol param')
-    @contract_method.param(:p5, :hash, 'Hash param')
-    @contract_method.param(:p6, :bool, 'Boolean param')
+    @contract_method.param(:str, :string, 'String param')
+    @contract_method.param(:int, :integer, 'Integer param')
+    @contract_method.param(:flt, :float, 'Float param')
+    @contract_method.param(:sym, :symbol, 'Symbol param')
+    @contract_method.param(:hsh, :hash, 'Hash param')
+    @contract_method.param(:bln, :bool, 'Boolean param')
     @contract_method.build_argument_schema
   end
 
   def test_parameter_types
     assert_equal :ok,
       @contract_method.invoke(
-        p1: 'hi',
-        p2: 8,
-        p3: 8.8,
-        p4: :ok,
-        p5: { a: 'nested', hash: 'param' },
-        p6: true,
+        str: 'hi',
+        int: 8,
+        flt: 8.8,
+        sym: :ok,
+        hsh: { a: 'nested', hash: 'param' },
+        bln: true,
       )
 
     wrong_args = assert_raises ArgumentError do
       @contract_method.invoke(the: 'wrong', arguments: true)
     end
-    assert wrong_args.message.include?('Argument [:p1] is missing')
+    assert wrong_args.message.include?('Argument [:str] is missing')
 
     wrong_types = assert_raises ArgumentError do
-      @contract_method.invoke(p1: 4)
+      @contract_method.invoke(str: 4)
     end
-    assert wrong_types.message.include?('Argument [:p1] must be a string')
+    assert wrong_types.message.include?('Argument [:str] must be a string')
   end
 
   class TestMethod
